a50a717005bc1db74b5bcecfd4cbef7397523caa
RobertoPatino1 <rapatino@espol.edu.ec>
2023-08-21 16:03:34 -0500

    Added lista filter by user id

--- routes/rest_lista.js
+++ routes/rest_lista.js
@@ -14,4 +14,27 @@ router.get("/findAll/json", function (req, res, next) {
     })
     .catch((error) => res.status(400).send(error));
 });
+
+//Find by UsuarioID
+router.get("/findByUsuarioId/:id/json", function (req, res, next) {
+  let usuarioId = parseInt(req.params.id);
+
+  lista
+    .findAll({
+      where: {
+        Usuario_idUsuario: usuarioId,
+      },
+      attributes: { exclude: ["updatedAt", "createdAt"] },
+    })
+    .then((instancia) => {
+      if (instancia) {
+        res.status(200).json(instancia);
+      } else {
+        res
+          .status(404)
+          .json({ error: "No existe registro con el identificador " + id });
+      }
+    })
+    .catch((error) => res.status(400).send(error));
+});
 module.exports = router;
